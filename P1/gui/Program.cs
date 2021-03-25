b848b81bddbb03b95008570399f331085bc411cd
owowpp1 <owowpp1@users.noreply.github.com>
2021-03-25 20:36:38 +0100

    przemeblowanko

---
+++ P1/gui/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gui
+{
+    static class gui
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        internal static void ustaf(int seed1, int seed2, int miejsca)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
